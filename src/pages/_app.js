4a2128e453a14a2a584e19a47aaaa635c491cac1
goyalVK <vkgoyal.vk85@gmail.com>
2026-04-28 12:43:40 +0530

    gsc code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import DeliveryBanner from '../components/DeliveryBanner';
@@ -14,6 +15,9 @@ function ScrollToTop() {
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="google-site-verification" content="aRCVs6Uz3vcDTacP2AQrWWjkuoEgB2ZmVz-wm-lOzz4" />
+      </Head>
       <ScrollToTop />
       <DeliveryBanner />
       <Navbar />
